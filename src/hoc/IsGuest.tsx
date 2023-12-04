fd7ca56a523101f0e4de0caf4379880a1e58b510
jatinnegi <negijatinn@gmail.com>
2023-12-04 12:14:38 +0530

    Fix hoc errors

--- src/hoc/IsGuest.tsx
+++ src/hoc/IsGuest.tsx
@@ -1,18 +1,21 @@
-import { NextPage } from "next";
 import { RootState } from "@/redux/reducers";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-const IsGuest = (Component: NextPage) => (props: any) => {
-  const { token, user } = useSelector((state: RootState) => state.user);
-  const router = useRouter();
+const IsGuest = (Component: any) => {
+  const AuthenticatedComponent = () => {
+    const { token, user } = useSelector((state: RootState) => state.user);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (token && user) router.push("/dashboard");
-  }, [token, user]);
+    useEffect(() => {
+      if (token && user) router.push("/dashboard");
+    }, [token, user]);
 
-  return <Component {...props} />;
+    return <Component />;
+  };
+
+  return AuthenticatedComponent;
 };
 
 export default IsGuest;
--- src/hoc/WithAuth.tsx
+++ src/hoc/WithAuth.tsx
@@ -1,22 +1,25 @@
-import { NextPage } from "next";
 import { RootState } from "@/redux/reducers";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-const WithAuth = (Component: NextPage) => (props: any) => {
-  const { token, user } = useSelector((state: RootState) => state.user);
-  const router = useRouter();
-  const [mounted, setMounted] = useState<boolean>(false);
+const WithAuth = (Component: any) => {
+  const AuthenticatedComponent = () => {
+    const { token, user } = useSelector((state: RootState) => state.user);
+    const router = useRouter();
+    const [mounted, setMounted] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (!token || !user) router.push("/");
-    else setMounted(true);
-  }, [token, user]);
+    useEffect(() => {
+      if (!token || !user) router.push("/");
+      else setMounted(true);
+    }, [token, user]);
 
-  if (!mounted) return <></>;
+    if (!mounted) return <></>;
 
-  return <Component {...props} />;
+    return <Component />;
+  };
+
+  return AuthenticatedComponent;
 };
 
 export default WithAuth;
